b3d0e14791e7a11c2ae7d9c67adfb7c2275e83c1
Corneel <corneelbooysen@hotmail.com>
2019-06-12 01:18:53 -0500

    Changes for playlist

---
+++ Plugins/EvaTTSPlugin/Intermediate/Build/Win64/UE4Editor/Inc/EvaTTSPlugin/EvaTTSPluginActor.generated.h
@@ -0,0 +1,88 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef EVATTSPLUGIN_EvaTTSPluginActor_generated_h
+#error "EvaTTSPluginActor.generated.h already included, missing '#pragma once' in EvaTTSPluginActor.h"
+#endif
+#define EVATTSPLUGIN_EvaTTSPluginActor_generated_h
+
+#define EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_RPC_WRAPPERS
+#define EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAEvaTTSPluginActor(); \
+	friend struct Z_Construct_UClass_AEvaTTSPluginActor_Statics; \
+public: \
+	DECLARE_CLASS(AEvaTTSPluginActor, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/EvaTTSPlugin"), NO_API) \
+	DECLARE_SERIALIZER(AEvaTTSPluginActor)
+
+
+#define EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAEvaTTSPluginActor(); \
+	friend struct Z_Construct_UClass_AEvaTTSPluginActor_Statics; \
+public: \
+	DECLARE_CLASS(AEvaTTSPluginActor, AActor, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/EvaTTSPlugin"), NO_API) \
+	DECLARE_SERIALIZER(AEvaTTSPluginActor)
+
+
+#define EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEvaTTSPluginActor(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEvaTTSPluginActor) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEvaTTSPluginActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEvaTTSPluginActor); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEvaTTSPluginActor(AEvaTTSPluginActor&&); \
+	NO_API AEvaTTSPluginActor(const AEvaTTSPluginActor&); \
+public:
+
+
+#define EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEvaTTSPluginActor(AEvaTTSPluginActor&&); \
+	NO_API AEvaTTSPluginActor(const AEvaTTSPluginActor&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEvaTTSPluginActor); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEvaTTSPluginActor); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AEvaTTSPluginActor)
+
+
+#define EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_PRIVATE_PROPERTY_OFFSET
+#define EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_9_PROLOG
+#define EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_RPC_WRAPPERS \
+	EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_INCLASS \
+	EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_PRIVATE_PROPERTY_OFFSET \
+	EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_INCLASS_NO_PURE_DECLS \
+	EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID EvaTTS_Code_Plugins_EvaTTSPlugin_Source_EvaTTSPlugin_Public_EvaTTSPluginActor_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
